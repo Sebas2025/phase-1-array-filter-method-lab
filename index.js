19ab90dac96856e9e1b888a7cf63f0e545dd0fbf
SebastainBernal <sebasair2025@gmail.com>
2022-12-19 14:48:10 -0700

    commit tree d55dd2fbb2ecae9b432253fdf910b5c67c8604a8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+
+
+function findMatching(driver, list) {
+    return driver.filter(
+     (possibleMatch) => possibleMatch.toLowerCase() === list.toLowerCase()
+    );
+}
+
+function fuzzyMatch(driver, list) {
+    return driver.filter(
+         (possibleMatch) => possibleMatch.toLowerCase().indexOf(list.toLowerCase())===0
+ );}
+
+
+ function matchName(driver, list){
+    return driver.filter((possibleMatch) => possibleMatch.name === list);
+ }
+
+
+        
